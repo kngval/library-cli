1c03733bb9046765afef941c8c2709fd8acadd0d
kngval <bkvalteocs@gmail.com>
2024-09-28 22:54:08 +0800

    session handle

---
+++ SessionManager.cs
@@ -0,0 +1,12 @@
+
+
+// public static class SessionManager {
+//
+//   public static int? LoggedInUserId {get;private set;}
+//
+//   public static void setLoggedInUser(int id)
+//   {
+//     LoggedInUserId = id;
+//   }
+//   public static void Logout()
+// }
